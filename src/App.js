e44b6bd6154b92bd2d9d90df132485d5582715ce
ntnlgrbly <97358186+ntnlgrbly@users.noreply.github.com>
2022-02-17 16:13:55 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import { useEffect } from 'react';
+ import { AppApi } from './apiComps/apiApp';
+import './App.css';
+//  import AppDate from './dateComps/appDate';
+// import AppPerant from './perantCopms/appPerant';
+
+function App() {
+
+  return (
+    <div className="container">
+      <AppApi/>
+  {/* <AppPerant /> */}
+  {/* <AppDate/> */}
+    </div>
+  );
+}
+
+export default App;
